570cb75b1fc8041356a7e95f29f0f79cf04d8db4
akartik80 <akartik80@gmail.com>
2016-04-02 19:22:10 +0530

    UX modifications

--- home.php
+++ home.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="">
-	<title>LANDON - Free Bootstrap 3 Landing Page</title>
+	<title>FlashCharge</title>
 	 
 	<link href="css/bootstrap.css" rel="stylesheet">
 	 
@@ -18,6 +18,18 @@
  
 	<link rel="shortcut icon" href="images/favicon.png">
 	<script src="js/pace.js"></script>
+
+	<style>
+		a:hover {
+		    color:white;
+		    text-decoration:none;
+		    cursor:pointer;
+   		}
+
+   		a {
+    		text-decoration: none;
+		}
+	</style>
  
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,600' rel='stylesheet' type='text/css'>
 </head>
@@ -26,7 +38,11 @@
 	<! -- ******************** MASTHEAD SECTION ******************** -->	
 	<main id="top" class="masthead" role="main">
 		<div class="container">
-			<div class="logo"> <a href="home.php"><img src="images/logo.png" alt="logo"></a>
+			<div class="logo"> <!-- <a href="home.php"><img src="images/logo.png" alt="logo">
+			</a>-->
+
+			<h1><b>FlashCharge</b></h1>
+
 			</div>
  
 			<h1>Just enter your number <strong>and we will figure the rest</strong> <br>
@@ -36,10 +52,10 @@
 					<form class="form-horizontal" role="form" action="subscribe.php" id="subscribeForm" method="POST">
 						<div class="form-group">
 							<div class="col-md-7 col-sm-6 col-sm-offset-1 col-md-offset-0">
-								<input class="form-control input-lg" name="mobile" type="mobile" id="mobile" placeholder="Enter your number" data-validate="validate(required, email)" required="required">
+								<input class="form-control input-lg" name="mobile" type="tel" id="mobile" placeholder="Enter your number" data-validate="validate(required, email)" required="required">
 							</div>
 							<div class="col-md-5 col-sm-4">
-								<button type="submit" class="btn btn-success btn-lg">FLASHCHARGE</button>
+								<button type="submit" class="btn btn-success btn-lg">FLASHCHARGE NOW</button>
 							</div>
 						</div>
 					</form>
@@ -55,37 +71,46 @@
 
     <! -- ******************** FEATURES SECTION ******************** -->	
 	<div class="container" id="explore">
- 
-		
-		
-		<section class="row features">
+		<br><br><br><br>
+ 		<section class="row features">
 			<div class="col-sm-6 col-md-3">
 				<div class="thumbnail"> 
-					<img src="images/service_01.png" alt="analytics-icon">
-					<div class="caption">
-						<h3>Important Feature 1</h3>
-						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-					</div>
+					<a href = "home.php">
+					
+						<img src="images/service_01.png" alt="analytics-icon">
+						
+						<div class="caption">
+							<h3>Mobile Recharge</h3>
+						
+							<p></p>
+						</div>
+					
+					</a>
 				</div><! --/thumbnail -->
 			</div><! --/col-sm-6-->
  
 			<div class="col-sm-6 col-md-3">
-				<div class="thumbnail"> 
-					<img src="images/service_02.png" alt="analytics-icon">
-					<div class="caption">
-						<h3>Important Feature 2</h3>
-						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-					</div>
+				<div class="thumbnail">
+					<a href = "bill.php">
+						<img src="images/service_02.png" alt="analytics-icon">
+						<div class="caption">
+							<h3>Electricity Bill</h3>
+							<p></p>
+						</div>
+					</a>
 				</div><! --/thumbnail -->
 			</div><! --/col-sm-6-->
  
 			<div class="col-sm-6 col-md-3">
-				<div class="thumbnail"> 
-					<img src="images/service_03.png" alt="analytics-icon">
-					<div class="caption">
-						<h3>Important Feature 3</h3>
-						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-					</div>
+				<div class="thumbnail">
+					<a href="dth.php">
+						<img src="images/service_03.png" alt="analytics-icon">
+						<div class="caption">
+							<h3>DTH</h3>
+							<p></p>
+						</div>
+					</a> 
+					
 				</div><! --/thumbnail -->
 			</div><! --/col-sm-6-->
  
@@ -93,15 +118,55 @@
 				<div class="thumbnail"> 
 					<img src="images/service_04.png" alt="analytics-icon">
 					<div class="caption">
-						<h3>Important Feature 4</h3>
-						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+						<h3>Water Bill</h3>
+						<p></p>
+					</div>
+				</div><! --/thumbnail -->
+			</div><! --/col-sm-6-->
+
+			<div class="col-sm-6 col-md-3">
+				<div class="thumbnail"> 
+					<img src="images/service_05.png" alt="analytics-icon">
+					<div class="caption">
+						<h3>Landline</h3>
+						<p></p>
+					</div>
+				</div><! --/thumbnail -->
+			</div><! --/col-sm-6-->
+
+			<div class="col-sm-6 col-md-3">
+				<div class="thumbnail"> 
+					<img src="images/service_06.png" alt="analytics-icon">
+					<div class="caption">
+						<h3>Gas</h3>
+						<p></p>
+					</div>
+				</div><! --/thumbnail -->
+			</div><! --/col-sm-6-->
+
+			<div class="col-sm-6 col-md-3">
+				<div class="thumbnail"> 
+					<img src="images/service_07.png" alt="analytics-icon">
+					<div class="caption">
+						<h3>Metro</h3>
+						<p></p>
+					</div>
+				</div><! --/thumbnail -->
+			</div><! --/col-sm-6-->
+
+			<div class="col-sm-6 col-md-3">
+				<div class="thumbnail"> 
+					<img src="images/service_08.png" alt="analytics-icon">
+					<div class="caption">
+						<h3>Bus</h3>
+						<p></p>
 					</div>
 				</div><! --/thumbnail -->
 			</div><! --/col-sm-6-->
 		</section><! --/section -->
  
 
-	    <! -- ******************** CLIENTS SECTION ******************** -->	 
+	    <! -- ******************** CLIENTS SECTION ******************** -->
 		<div class="section-title">
 			<h2>Some of our partners</h2>
 			<h4>We work with a variety of merchants</h4>
@@ -109,58 +174,45 @@
 	
 		<section class="row clientlogo breath">
 			<div class="col-md-12 text-center"> 
-				<img src="img/paytm.jpeg" alt="client-logos">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-				<img src="img/freecharge.png" alt="client-logos">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-				<img src="img/mobikwik.jpg" alt="client-logos">
+				<a href="paytm.com"><img src="img/paytm.jpeg" alt="client-logos"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				<a href="freecharge.com"><img src="img/freecharge.png" alt="client-logos"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				<a href="mobikwik.com"><img src="img/mobikwik.jpg" alt="client-logos"></a>
 			</div>
 		</section><! --/section -->
- 
-	</div><! --/container -->
  
 
 	    <! -- ******************** TESTIMONIALS SECTION ******************** -->	 
 			  
  
-		<div class="section-title highlight-testimonials">
+		<div class="section-title" >
 			<h5>Frequently Asked Questions</h5>
 		</div>
 		
 		<section class="row faq breath">
 			<div class="col-md-6">
-				<h6>How does the free trial work?</h6>
-				<p>Our 10 day trial is 100% free and does not require credit card information to start. If at the end of your trial you would like to upgrade, great. If not, you can cancel your account altogether, or let us mark it as inactive for you to come back to later.</p>
-				<h6>Can I switch plans later?</h6>
-				<p>Absolutely. You can switch between our paid plans, or cancel your account altogether, whenever you like. We will adjust any payments accordingly.</p>
+				<h6>What do I have to do?</h6>
+				<p>Welcome to FlashCharge. Get your recharge done in a flash! All you got to do is to provide your mobile number and we 
+                    will bring out the best recharge offer for you, everything in a flash!</p>
+				<h6>Can I pay bills?</h6>
+				<p>Absolutely! FlashCharge picks out best deals for your electricity, water bills and even metro recharges too</p>
 			</div><! --/col-md-6 -->
 			
 			<div class="col-md-6">
-				<h6>Do I need to choose a plan now?</h6>
-				<p>No. You get the full featured, unlimited version of our service completely free for 10 days. Once you're ready to upgrade, you may choose a plan which suits your needs.</p>
-				<h6>What payment types do you accept?</h6>
-				<p>We accept payments from MasterCard, Visa, Visa Debit and American Express. We do not accept PayPal. Remember, you do not need to supply card details to start your free trial.</p>
+				<h6>How it works</h6>
+				<p>Ever had a hussle in comparing recharge coupons? Indeed. Everybody did. But not anymore. We compare coupons available on top websites to pick out the best for you </p>
+				<h6>How much I have to pay?</h6>
+				<p>No, nothing. Yes you read it right, FlashCharge is absolutely free of cost. Just sign up and enjoy recharging at lowest prices.</p>
 			</div><! --/col-md-6 -->
 		</section><! --/section faq -->
  
 	</div><! --/container -->
  
-	    <! -- ******************** FOOTER ******************** -->	  
-		<main class="footercta" role="main">
-			<div class="container">
-				<h1>The Most Simple & <strong>Powerful Way</strong> <br>
-					to <strong>launch</strong> your product</h1>
- 
-				<div class="row">
-					<div class="col-md-12 breath text-center"> 
-						<a href="index.html#top" class="btn btn-success btn-lg gototop">GET EARLY ACCESS</a> 
-					</div>
-				</div>
-			</div><! --/container -->
-		</main><! --/main -->
+	    
  
 		<div class="container">
 			<section class="row breath">
 				<div class="col-md-12 footerlinks">
-					<p>&copy; 2014 BlackTie.co. All Rights Reserved</p>
+					<p>&copy; www.flashcharge.com All Rights Reserved</p>
 				</div>
 			</section><! --/section -->
 		</div><! --/container -->
